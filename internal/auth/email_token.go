525613b2d7dfe73394a5897729f4966433c03f2f
Willem Schots <w@willem.dev>
2024-03-25 14:38:53 +0100

    4-account-registrations: email token implementation

--- internal/auth/email_token.go
+++ internal/auth/email_token.go
@@ -1,12 +1,22 @@
 package auth
 
 import (
+	"encoding/hex"
+	"errors"
+	"log/slog"
 	"time"
 
 	"github.com/willemschots/househunt/internal/email"
 )
 
-// EmailToken contains the data for an email token.
+const (
+	tokenLen = 32
+)
+
+var ErrInvalidToken = errors.New("invalid token")
+
+// EmailToken contains the state of a random token that is sent via email.
+// Such tokens should be only used once and have a limited lifetime.
 type EmailToken struct {
 	ID int
 	// TokenHash is the hash of the token. We hash the token to prevent someone with
@@ -26,3 +36,55 @@ const (
 	// EmailTokenPurposeActivate indicates an email token is for activating an account.
 	EmailTokenPurposeActivate EmailTokenPurpose = "activate"
 )
+
+// Token is a random token that is sent via email.
+//
+// The only time a token should be provided in plaintext is as part of
+// the email to the user. Tokens are confidential and should never be
+// exposed in logs or persisted in plaintext.
+type Token [tokenLen]byte
+
+// GenerateToken creates a new random token.
+func GenerateToken() (Token, error) {
+	b, err := genRandomBytes(tokenLen)
+	if err != nil {
+		return [tokenLen]byte{}, err
+	}
+	return [tokenLen]byte(b), nil
+}
+
+// ParseToken parses a token from a string.
+func ParseToken(raw string) (Token, error) {
+	if len(raw) != tokenLen*2 {
+		return [tokenLen]byte{}, ErrInvalidToken
+	}
+
+	b, err := hex.DecodeString(raw)
+	if err != nil {
+		return [tokenLen]byte{}, ErrInvalidToken
+	}
+
+	return [tokenLen]byte(b), nil
+}
+
+// String returns the string representation of the token.
+// As opposed to a Password this allowed, we need to embed the
+// token in emails.
+func (t Token) String() string {
+	return hex.EncodeToString(t[:])
+}
+
+// Hash hashes the token using the argon2id algorithm.
+func (t Token) Hash() (Argon2Hash, error) {
+	return hashBytes(t[:])
+}
+
+// Match checks if the token matches the given hash.
+func (t Token) Match(h Argon2Hash) bool {
+	return matchHash(h, t[:])
+}
+
+// LogValue implements the slog.Valuer interface.
+func (t Token) LogValue() slog.Value {
+	return slog.StringValue(SecretMarker)
+}
--- internal/auth/password.go
+++ internal/auth/password.go
@@ -1,11 +1,7 @@
 package auth
 
 import (
-	"crypto/rand"
-	"crypto/subtle"
 	"fmt"
-
-	"golang.org/x/crypto/argon2"
 )
 
 const (
@@ -18,8 +14,8 @@ const (
 	maxPasswordBytes = 512
 
 	// SecretMarker is a string we can look for in logs to see if the app
-	// is accidentally exposing passwords.
-	SecretMarker = "<!PASSWORD_REDACTED!>"
+	// is accidentally exposing secrets.
+	SecretMarker = "<!SECRET_REDACTED!>"
 )
 
 var ErrInvalidPassword = fmt.Errorf("invalid password")
@@ -51,43 +47,12 @@ func ParsePassword(pwd string) (Password, error) {
 
 // Match checks if the plaintext password matches the given hash.
 func (p Password) Match(h Argon2Hash) bool {
-	// Hash the plaintext password with the same parameters as the provided hash.
-	other := argon2.IDKey(p.plain, h.Salt, h.Iterations, h.MemoryKiB, h.Parallelism, uint32(len(h.Hash)))
-
-	// use subtle to compare the two hashes in constant time to avoid timing attacks.
-	return subtle.ConstantTimeCompare(other, h.Hash) == 1
+	return matchHash(h, p.plain)
 }
 
 // Hash hashes the plaintext password using the argon2id algorithm.
 func (p Password) Hash() (Argon2Hash, error) {
-	// First we generate a salt.
-	salt, err := generateSalt()
-	if err != nil {
-		return Argon2Hash{}, fmt.Errorf("failed to generate salt: %w", err)
-	}
-
-	// Then we hash the password.
-	hash := argon2.IDKey(p.plain, salt, iterations, memoryKiB, parallelism, keyLen)
-
-	return Argon2Hash{
-		Variant:     variant,
-		Version:     argon2.Version,
-		MemoryKiB:   memoryKiB,
-		Iterations:  iterations,
-		Parallelism: parallelism,
-		Salt:        salt,
-		Hash:        hash,
-	}, nil
-}
-
-func generateSalt() ([]byte, error) {
-	salt := make([]byte, saltLen)
-	_, err := rand.Read(salt)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read random bytes: %w", err)
-	}
-
-	return salt, nil
+	return hashBytes(p.plain)
 }
 
 func (p Password) String() string {
