93809448e794fb98c594cfd8b5a1233cc82068d2
Willem Schots <w@willem.dev>
2024-03-26 16:09:51 +0100

    4-failure-tests: failing store tests

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -11,11 +11,12 @@ import (
 	"github.com/willemschots/househunt/internal/auth/db"
 	"github.com/willemschots/househunt/internal/db/testdb"
 	"github.com/willemschots/househunt/internal/email"
+	"github.com/willemschots/househunt/internal/errorz/testerr"
 )
 
 func Test_Service_RegisterAccount(t *testing.T) {
 	setup := func(t *testing.T) (*auth.Service, *errList) {
-		testDB := db.New(testdb.RunWhile(t, true), func() time.Time {
+		store := db.New(testdb.RunWhile(t, true), func() time.Time {
 			return time.Now().Round(0)
 		})
 
@@ -23,9 +24,29 @@ func Test_Service_RegisterAccount(t *testing.T) {
 			mutex: &sync.Mutex{},
 			errs:  make([]error, 0),
 		}
-		svc := auth.NewService(testDB, errs.AppendErr, time.Second)
+
+		svc := auth.NewService(store, errs.AppendErr, time.Second)
 
 		return svc, errs
+	}
+
+	setupFailingStore := func(t *testing.T, dep *testerr.FailingDep) (*auth.Service, *errList) {
+		store := &failingStore{
+			store: db.New(testdb.RunWhile(t, true), func() time.Time {
+				return time.Now().Round(0)
+			}),
+			dep: dep,
+		}
+
+		errs := &errList{
+			mutex: &sync.Mutex{},
+			errs:  make([]error, 0),
+		}
+
+		svc := auth.NewService(store, errs.AppendErr, time.Second)
+
+		return svc, errs
+
 	}
 
 	t.Run("ok, register account", func(t *testing.T) {
@@ -74,6 +95,28 @@ func Test_Service_RegisterAccount(t *testing.T) {
 
 		// TODO: Assert only a single email was send.
 	})
+
+	for _, dep := range testerr.NewFailingDeps(testerr.Err, 5) {
+		t.Run("fail, dependency fails", func(t *testing.T) {
+			svc, errs := setupFailingStore(t, &dep)
+
+			credentials := auth.Credentials{
+				Email:    emailAddress(t, "test@example.com"),
+				Password: password(t, "reallyStrongPassword1"),
+			}
+			err := svc.RegisterAccount(context.Background(), credentials)
+			if err != nil {
+				t.Fatalf("failed to register account: %v", err)
+			}
+
+			// Wait for service goroutines to finish.
+			svc.Close()
+
+			errs.assertErrorIs(t, testerr.Err)
+
+			// TODO: Assert no email was sent.
+		})
+	}
 }
 
 type errList struct {
@@ -133,4 +176,76 @@ func password(t *testing.T, raw string) auth.Password {
 	}
 
 	return p
+}
+
+// failingStore wraps a real store but fails on specific calls.
+//
+// The failure is controlled by a FailingDep. They either fail
+// on the xth call, or on the xth call and all calls after that.
+type failingStore struct {
+	store auth.Store
+	dep   *testerr.FailingDep
+}
+
+func (f *failingStore) BeginTx(ctx context.Context) (auth.Tx, error) {
+	return testerr.MaybeFail(f.dep, func() (auth.Tx, error) {
+		realTx, err := f.store.BeginTx(ctx)
+		return &failingTx{
+			store: f,
+			tx:    realTx,
+		}, err
+	})
+}
+
+type failingTx struct {
+	store *failingStore
+	tx    auth.Tx
+}
+
+func (f *failingTx) Commit() error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.Commit()
+	})
+}
+
+func (f *failingTx) Rollback() error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.Rollback()
+	})
+}
+
+func (f *failingTx) CreateUser(u *auth.User) error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.CreateUser(u)
+	})
+}
+
+func (f *failingTx) UpdateUser(u *auth.User) error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.UpdateUser(u)
+	})
+}
+
+func (f *failingTx) FindUserByEmail(v email.Address) (auth.User, error) {
+	return testerr.MaybeFail(f.store.dep, func() (auth.User, error) {
+		return f.tx.FindUserByEmail(v)
+	})
+}
+
+func (f *failingTx) CreateEmailToken(t *auth.EmailToken) error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.CreateEmailToken(t)
+	})
+}
+
+func (f *failingTx) UpdateEmailToken(t *auth.EmailToken) error {
+	return testerr.MaybeFailErrFunc(f.store.dep, func() error {
+		return f.tx.UpdateEmailToken(t)
+	})
+}
+
+func (f *failingTx) FindEmailTokenByID(id int) (auth.EmailToken, error) {
+	return testerr.MaybeFail(f.store.dep, func() (auth.EmailToken, error) {
+		return f.tx.FindEmailTokenByID(id)
+	})
 }
