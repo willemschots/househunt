ddff493ade3e5d93100ca806f868fbe2e192b328
Willem Schots <w@willem.dev>
2024-03-27 14:28:20 +0100

    4-sending-emails: email service

--- internal/email/service.go
+++ internal/email/service.go
@@ -1,7 +1,9 @@
 package email
 
 import (
+	"bytes"
 	"context"
+	"io"
 )
 
 // TemplateElement is used by a renderer to identify the different parts of an email template.
@@ -14,28 +16,44 @@ const (
 
 // Renderer is responsible for rendering email templates.
 type Renderer interface {
-	Render(ctx context.Context, name string, element TemplateElement, data any) (string, error)
+	Render(w io.Writer, name string, element TemplateElement, data any) error
 }
 
 // Sender is responsible for actually sending an email.
 type Sender interface {
-	Send(ctx context.Context, sender, recipient Address, subject, body string) error
+	Send(ctx context.Context, from, recipient Address, subject, body string) error
 }
 
 // Service provides the main functionality for sending emails.
 type Service struct {
+	from     Address
 	renderer Renderer
 	sender   Sender
 }
 
-func NewService(renderer Renderer, sender Sender) *Service {
+func NewService(from Address, renderer Renderer, sender Sender) *Service {
 	return &Service{
+		from:     from,
 		renderer: renderer,
 		sender:   sender,
 	}
 }
 
-func (s *Service) SendMessage(ctx context.Context, name string, recipient Address, data any) error {
-	// TODO: Implement.
-	return nil
+func (s *Service) Send(ctx context.Context, name string, recipient Address, data any) error {
+	var (
+		sBuf bytes.Buffer
+		bBuf bytes.Buffer
+	)
+
+	err := s.renderer.Render(&sBuf, name, ElementSubject, data)
+	if err != nil {
+		return err
+	}
+
+	err = s.renderer.Render(&bBuf, name, ElementBody, data)
+	if err != nil {
+		return err
+	}
+
+	return s.sender.Send(ctx, s.from, recipient, sBuf.String(), bBuf.String())
 }
